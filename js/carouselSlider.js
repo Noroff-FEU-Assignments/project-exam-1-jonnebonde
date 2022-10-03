745a5c0b0046374c631c1c7f829b7db7c3554de9
jonnebonde <jonnebonde@gmail.com>
2022-10-03 14:51:35 +0200

    carousel slider

--- js/carouselSlider.js
+++ js/carouselSlider.js
@@ -2,32 +2,21 @@ import {apiCall} from "../js/utils.js"
 import {url, carouselContainer} from "../js/constants.js"
 
 
+
+
+
 // API call //
 
+const sliderUrl = url + "&per_page=14";
 
 
 async function apiCarousel(){
 
     try{
-        const blogs = await apiCall(url)
+        const blogs = await apiCall(sliderUrl)
         console.log(blogs)
 
-        const date = new Date(blogs[1].date).toLocaleDateString("utc", {
-            year: "numeric",
-            month: "long",
-            day: "2-digit",
-        });
-
-        console.log(date)
-
-        carouselContainer.innerHTML = `
-        <span>Author: ${blogs[1]._embedded.author[0].name} Posted: ${date}</span>
-        <h1>${blogs[1].title.rendered}</h1>
-        <img src="${blogs[1]._embedded["wp:featuredmedia"][0].source_url}">
-        <div>${blogs[1].content.rendered}</div>
-        
-        `
-
+        makePostCarouselSLiderHtml(blogs)
 
     }
     catch{
@@ -36,3 +25,55 @@ async function apiCarousel(){
 }
 
 apiCarousel()
+
+/* blogpost carousel slider */
+
+const carouselContainers = [...document.querySelectorAll(".index-post-container")];
+const nextSlideButton = [...document.querySelectorAll(".next-btn")];
+const previousSlideButton = [...document.querySelectorAll(".previous-btn")];
+
+
+carouselContainers.forEach((item, i) => {
+    let containerDimensions = item.getBoundingClientRect();
+    let containerWidth = containerDimensions.width;
+
+    nextSlideButton[i].addEventListener("click", () => {
+        item.scrollLeft += containerWidth;
+    })
+
+    previousSlideButton[i].addEventListener("click", () => {
+        item.scrollLeft -= containerWidth;
+    })
+})
+
+
+/* making html */
+
+function makePostCarouselSLiderHtml(blogs) {
+
+    blogs.forEach(function (blogPosts) {
+
+        const date = new Date(blogPosts.date).toLocaleDateString("utc", {
+            year: "numeric",
+            month: "long",
+            day: "2-digit",
+        });
+
+
+     
+        carouselContainer.innerHTML += `
+
+        <div class="carousel-post">
+            <div class="carousel-post-text">
+            <span>${blogPosts.title.rendered}</span>
+            <span>Author: ${blogPosts._embedded.author[0].name}</span>
+            <span>Posted: ${date}</span>
+            <a href="*">read more</a>
+            </div>
+            <div>
+            <img src="${blogPosts._embedded["wp:featuredmedia"][0].source_url}">
+            </div>
+        `
+    })
+}
+
