4d5580032ec0cd7bb87f8d6b443e2343137e1495
jonnebonde <jonnebonde@gmail.com>
2022-11-05 20:42:51 +0100

    importing assignment

--- js/utils.js
+++
@@ -1,9 +0,0 @@
-import {url} from "../js/constants.js"
-
-
-export async function apiCall(url) {
-    const response = await fetch(url);
-    const result = await response.json();
-    return result
-}
-
